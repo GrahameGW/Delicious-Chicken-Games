cf34067a81514cfa247182f68b8a51e17dc54041
Grahame Watt <watt.grah@gmail.com>
2020-09-24 11:46:52 -0500

    Player controller, state scriptables

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/NPC")]
+public class NPC : ScriptableObject
+{
+    public new string name;
+    public float approvalScore = 0.0f;
+
+}

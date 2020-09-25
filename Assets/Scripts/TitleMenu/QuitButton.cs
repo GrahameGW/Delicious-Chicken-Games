5fc55c979658d54a5e64c939aaf7771d302808c7
Grahame Watt <watt.grah@gmail.com>
2020-09-25 11:11:39 -0500

    title screen and menu

---
+++ Assets/Scripts/TitleMenu/QuitButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace TitleScreen
+{
+    public class QuitButton : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject confirmQuitPanel = default;
+        [SerializeField]
+        private GameObject menuButtons = default;
+
+        private void Start()
+        {
+            confirmQuitPanel.SetActive(false);
+        }
+
+        public void QuitApp()
+        {
+            confirmQuitPanel.SetActive(true);
+            menuButtons.SetActive(false);
+        }
+
+        public void ConfirmQuit()
+        {
+            Application.Quit();
+        }
+
+        public void CancelQuit()
+        {
+            confirmQuitPanel.SetActive(false);
+            menuButtons.SetActive(true);
+        }
+    }
+}
+

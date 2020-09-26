eee7723317c2d6a6babcfd7ce53ed1867ee11b1f
Grahame Watt <watt.grah@gmail.com>
2020-09-26 11:38:48 -0500

    click through to broadcast

---
+++ Assets/Scripts/DeskComputer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeskComputer : InteractiveObject
+{
+    [SerializeField] InteractiveObject[] otherComponents = default;
+
+    protected override void Execute()
+    {
+        SceneManager.LoadScene("CastSchedule");
+    }
+
+    protected override void OnMousePosChangeListener(Vector2 position)
+    {
+        bool wasHighlighted = isHighlighted;
+
+        base.OnMousePosChangeListener(position);
+
+        if (isHighlighted && !wasHighlighted) {
+            HighlightOthers();
+        }
+        else if (wasHighlighted && !isHighlighted)
+            DeHighlightOthers();
+        }
+
+
+    private void HighlightOthers()
+    {
+        for (int i = 0; i < otherComponents.Length; i++) {
+            otherComponents[i].Highlight();
+        }   
+    }
+    private void DeHighlightOthers()
+    {
+        for (int i = 0; i < otherComponents.Length; i++) {
+            otherComponents[i].DeHighlight();
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public abstract class InteractiveObject : MonoBehaviour
+{
+    protected bool isHighlighted = false;
+
+    [SerializeField] Sprite atRestSprite = default;
+    [SerializeField] Sprite highlightSprite = default;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = atRestSprite;
+        InputManager.Instance.OnMousePositionChange.AddListener(OnMousePosChangeListener);
+    }
+    private void OnDestroy()
+    {
+        if (InputManager.Instance != null)
+            InputManager.Instance.OnMousePositionChange.RemoveListener(OnMousePosChangeListener);
+    }
+    private void Update()
+    {
+        if (isHighlighted && Input.GetButtonDown("Fire1")) {
+            Execute();
+        }
+    }
+
+    public void Highlight()
+    {
+        spriteRenderer.sprite = highlightSprite;
+        isHighlighted = true;
+    }
+    public void DeHighlight()
+    {
+        spriteRenderer.sprite = atRestSprite;
+        isHighlighted = false;
+    }
+
+    protected virtual void OnMousePosChangeListener(Vector2 position)
+    {
+        if (spriteRenderer.sprite.bounds.Contains(position)) {
+            Highlight();
+        }
+        else {
+            DeHighlight();
+        }
+    }
+
+    protected abstract void Execute();
+}
+
+

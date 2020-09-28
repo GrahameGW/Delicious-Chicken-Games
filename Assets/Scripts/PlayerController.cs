5a8c1e1cb5b3c9ddf2f9f448bf69a2ad2bb88234
LueptowK <kylelueptow@gmail.com>
2020-09-28 00:35:47 -0500

    Psuedo pathfinding
    simple naive "pathfinding" that'll take the player around unwalkable objects (but will let them get stuck in corners)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -39,9 +39,39 @@ public class PlayerController : MonoBehaviour
         Vector2 direction = (destination - (Vector2)transform.position).normalized;
         spriteRenderer.flipX = direction.x < 0;
 
-        while (Vector2.Distance(transform.position, destination) >= 0.05f) {
-            transform.Translate(direction * Time.deltaTime * speed);
-            transform.localScale = ScaleWithDistance(transform.position);
+        while (Vector2.Distance(transform.position, destination) >= 0.05f)
+        {
+            if (walkableBounds.OverlapPoint((Vector2)transform.position + (direction * Time.deltaTime * speed)))
+            {
+                transform.Translate(direction * Time.deltaTime * speed);
+                transform.localScale = ScaleWithDistance(transform.position);
+            }
+            else
+            {
+                //If blindly going in the direction we'd like to get to takes us outside of walkable bounds, try going around
+                if (!walkableBounds.OverlapPoint(transform.position)) Debug.LogError("Player outside of walkable bounds");
+
+                RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + (direction * Time.deltaTime * speed), -direction, 1f, LayerMask.GetMask("WalkBounds"));
+
+                if (hit.collider == null) { Debug.LogError("No valid collision from player controller walk bounds."); }
+                else
+                {
+                    //Cross product of walk bounds normal and the z vector gets us a vector along the collider's side
+                    Vector3 tempDirection = Vector3.Cross(hit.normal, Vector3.forward);
+
+                    //Dot product of that direction lets us direct naively in the direction that takes us more towards our target
+                    if (Vector3.Dot(tempDirection, direction) < 0) tempDirection = -tempDirection;
+
+                    //if this doesn't work, we just stop, since we're in a corner
+                    if (!walkableBounds.OverlapPoint(transform.position + (tempDirection * Time.deltaTime * speed))) { break; }
+                    else
+                    {
+                        transform.Translate(tempDirection * Time.deltaTime * speed);
+                        transform.localScale = ScaleWithDistance(transform.position);
+                    }
+                }
+            }
+            
             yield return null;
         }
         animator.SetBool("walking", false);

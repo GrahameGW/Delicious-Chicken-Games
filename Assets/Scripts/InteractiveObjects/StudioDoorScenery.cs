e0fa795751c65c0ec913d8de1e0572a4007a496b
Grahame Watt <watt.grah@gmail.com>
2020-09-28 17:56:32 -0500

    loop done

--- Assets/Scripts/InteractiveObjects/StudioDoorScenery.cs
+++ Assets/Scripts/InteractiveObjects/StudioDoorScenery.cs
@@ -1,25 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StudioDoorScenery : MonoBehaviour
+public class StudioDoorScenery : InteractiveObject
 {
-    [SerializeField] GlobalState globalState = default;
     [SerializeField] Sprite daySprite = default;
+    [SerializeField] Sprite dayHighlightedSprite = default;
     [SerializeField] Sprite nightSprite = default;
+    [SerializeField] Sprite nightHighlightedSprite = default;
+    [SerializeField] string nightAction = default;
+    [SerializeField] string dayAction = default;
 
-    private SpriteRenderer spriteRenderer;
-
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
 
     private void OnEnable()
     {
         if (globalState.currentTime == TimeOfDay.Morning) {
-            spriteRenderer.sprite = daySprite;
+            spriteRenderer.sprite = atRestSprite = daySprite;
+            highlightSprite = dayHighlightedSprite;
+            actionDescription = dayAction;
         }
         if (globalState.currentTime == TimeOfDay.Evening) {
-            spriteRenderer.sprite = nightSprite;
+            spriteRenderer.sprite = atRestSprite = nightSprite;
+            highlightSprite = nightHighlightedSprite;
+            actionDescription = nightAction;
+        }
+    }
+
+    public override void Execute()
+    {
+        if (globalState.currentTime == TimeOfDay.Evening) {
+            SceneManager.LoadScene("EndOfDay");
         }
     }
 }
---
+++ Assets/Scripts/NextDay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextDay : MonoBehaviour
+{
+    [SerializeField] GlobalState globalState = default;
+    
+    public void Next()
+    {
+        globalState.currentDay++;
+        globalState.currentTime = TimeOfDay.Morning;
+    }
+}

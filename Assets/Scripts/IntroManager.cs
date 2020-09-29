b2ee4781da9a411ab06b3ba326f2118a5b456fe6
LueptowK <kylelueptow@gmail.com>
2020-09-29 11:24:07 -0500

    Intro sequence yarn stuff

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+using Yarn;
+
+public class IntroManager : MonoBehaviour
+{
+    [SerializeField] DialogueRunner dialogueRunner;
+    [SerializeField] YarnProgram yarnDialogue;
+    [SerializeField] string nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueRunner.Add(yarnDialogue);
+        dialogueRunner.StartDialogue();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

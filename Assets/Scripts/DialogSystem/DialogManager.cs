a783c37ed9bc736e03bc40a2d3e787e60566f44c
LueptowK <kylelueptow@gmail.com>
2020-09-29 19:46:37 -0500

    Adds SetSpeaker for character portraits in dialogue

--- Assets/Scripts/DialogSystem/DialogManager.cs
+++ Assets/Scripts/DialogSystem/DialogManager.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Yarn.Unity;
 
 public class DialogManager : MonoBehaviour
 {
+    [SerializeField] DialogueRunner dialogueRunner;
+    [SerializeField] List<Sprite> speakerSprites;
+    [SerializeField] List<string> speakerNames;
+    [SerializeField] Image speakerPortrait;
+
     /// <summary>
     /// These events have been linked to the DialogueUI script to trigger on dialog start and stop.
     /// </summary>
     #region Dialog_UI_Events
     public static event Action DialogStarted;
     public static event Action DialogStopped;
+
+    private void Awake()
+    {
+        dialogueRunner.AddCommandHandler("SetSpeaker", SetSpeakerName);
+    }
 
     public void StartDialog()
     {
@@ -18,6 +31,16 @@ public class DialogManager : MonoBehaviour
     public void StopDialog()
     {
         DialogStopped?.Invoke();
+    }
+
+    void SetSpeakerName(string[] parameters)
+    {
+        speakerPortrait.gameObject.SetActive(true);
+        if (speakerNames.IndexOf(parameters[0]) == -1)
+        {
+            Debug.LogError("Could not find portrait for " + parameters[0]);
+        }
+        speakerPortrait.sprite = speakerSprites[speakerNames.IndexOf(parameters[0])];
     }
 
     #endregion

ee90cd7ac96dee8a1f5d40bac591fea8e4d8e9f1
LueptowK <kylelueptow@gmail.com>
2020-09-30 11:49:46 -0500

    adds StartNodes to dialogue organizer

--- Assets/Scripts/DialogueOrganizer.cs
+++ Assets/Scripts/DialogueOrganizer.cs
@@ -11,27 +11,33 @@ public class DialogueOrganizer : ScriptableObject
     {
         [SerializeField] YarnProgram morningDialogue;
         [SerializeField] string morningLocation;
+        [SerializeField] string morningStartNode;
         [SerializeField] YarnProgram eveningDialogue;
         [SerializeField] string eveningLocation;
+        [SerializeField] string eveningStartNode;
 
         public YarnProgram MorningDialogue { get => morningDialogue; set => morningDialogue = value; }
         public string MorningLocation { get => morningLocation; set => morningLocation = value; }
         public YarnProgram EveningDialogue { get => eveningDialogue; set => eveningDialogue = value; }
         public string EveningLocation { get => eveningLocation; set => eveningLocation = value; }
+        public string MorningStartNode { get => morningStartNode; set => morningStartNode = value; }
+        public string EveningStartNode { get => eveningStartNode; set => eveningStartNode = value; }
     }
 
-    public void UpdateDialogueForDay(int day, YarnProgram dialogue, string time, string location)
+    public void UpdateDialogueForDay(int day, YarnProgram dialogue, string time, string location, string startNode)
     {
         DialoguePerDay dialoguePerDay = dialoguesPerDays[day];
         if (time == "morning")
         {
             dialoguePerDay.MorningDialogue = dialogue;
             dialoguePerDay.MorningLocation = location;
+            dialoguePerDay.MorningStartNode = startNode;
         }
         else if (time == "evening")
         {
             dialoguePerDay.EveningDialogue = dialogue;
             dialoguePerDay.EveningLocation = location;
+            dialoguePerDay.EveningStartNode = startNode;
         }
         else
         {

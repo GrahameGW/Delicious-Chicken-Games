986a8adf042a438b99288a276b903a1481c2b857
Grahame Watt <watt.grah@gmail.com>
2020-10-03 17:09:23 -0500

    fix intro sequence

--- Assets/Scripts/Arcs/BucksArc.cs
+++ Assets/Scripts/Arcs/BucksArc.cs
@@ -3,13 +3,10 @@
 [CreateAssetMenu(menuName = "Arc State/Bucks's Arc")]
 public class BucksArc : Arc
 {
-    [SerializeField] DialogueOrganizer organizer = default;
     [SerializeField] BroadcastItems interviews = default;
     [SerializeField] BroadcastClip[] bucksClips = new BroadcastClip[3];
-    [SerializeField] GlobalState state = default;
 
     public bool hasMet = false;
-    public int score = 0;
     public int currentClip = 0;
 
     public override void OnBroadcast(BroadcastClip clip, int day)

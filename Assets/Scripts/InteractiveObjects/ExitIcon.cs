09d9b0d6535f2a5554d625c343adec4c5a5bd296
Grahame Watt <watt.grah@gmail.com>
2020-10-03 10:37:43 -0500

    new art and updates

---
+++ Assets/Scripts/InteractiveObjects/ExitIcon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitIcon : InteractiveObject
+{
+    [SerializeField] ScheduleManager scheduleManager = default;
+
+    public override void Execute()
+    {
+        scheduleManager.Save();
+        actionDescription = scheduleManager.GetSaved();
+        actionText.text = actionDescription;
+    }
+}
---
+++ Assets/Scripts/InteractiveObjects/SaveIcon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveIcon : InteractiveObject
+{
+    public override void Execute()
+    {
+
+    }
+}
